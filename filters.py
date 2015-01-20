61c58fd1b5247ba1b8805dc930dafe2fadffd816
Sami Koho <sami.koho@gmail.com>
2015-01-20 15:04:11 +0100

    Image quality stuff. Code on frequency estimation added. Still far from ready but the idea is there. Everything is very much un-tested at this stage.

--- filters.py
+++ filters.py
@@ -1,7 +1,9 @@
 import numpy
-from scipy import ndimage
+from scipy import ndimage, fftpack
+from matplotlib import pyplot as plt
 
 from image import Image
+import External.radial_profile as radprof
 
 
 class Filter(object):
@@ -88,7 +90,7 @@ class LocalImageQuality(Filter):
         peaks = numpy.percentile(self.data_temp, 85)
         return numpy.where(self.data_temp >= peaks, 1, 0)
 
-    def calculate_image_quality(self, kernel=None):
+    def calculate_image_quality(self, kernel=None, show=False):
         if kernel is not None:
             self.set_smoothing_kernel_size(kernel)
 
@@ -100,9 +102,64 @@ class LocalImageQuality(Filter):
         positions = self.find_sampling_positions()
         self.data_temp = self.data[:][numpy.nonzero(positions)]
 
-        Image(self.data[:]*positions, self.spacing).show()
+        if show:
+            Image(self.data[:]*positions, self.spacing).show()
 
         return self.calculate_entropy()
+
+
+class ImageResolution(Filter):
+    def __init__(self, image, physical=False):
+
+        Filter.__init__(self, image, physical)
+
+        self.average = None
+        self.power = None
+        self.kernel_size = []
+
+    def set_image(self, image):
+        self.data = image
+
+    def calculate_power_spectrum(self, show=False):
+        self.power = numpy.abs(fftpack.fftshift(fftpack.fft2(self.data)))**2
+        if show:
+            Image(numpy.log10(self.power), self.spacing).show()
+
+    def calculate_azimuthal_average(self, show=False):
+        bin_centers, average = radprof.azimuthalAverage(
+            self.power,
+            returnradii=True,
+            normalize=True
+        )
+        dx = self.data.get_spacing()[0]
+        dim = self.data.get_dimensions()[0]
+
+        f_k = 2*bin_centers/(dx*dim)
+
+        self.average = [average, f_k]
+
+        if show:
+            plt.plot(f_k, self.average)
+            plt.ylabel("Average power")
+            plt.xlabel("Frequency")
+            plt.show()
+
+    def calculate_image_resolution(self, show_intermediate=False, show=False, measure="quartal"):
+        """
+        The calculated resolution in the histogram depends on the number of histogram bins. I have to convert
+        the bins into physical distances.
+        """
+        assert self.data is not None, "Please set an image to process"
+        self.calculate_power_spectrum(show=show_intermediate)
+        self.calculate_azimuthal_average(show=show_intermediate)
+
+        if measure is "quartal":
+            pos = numpy.percentile(self.average[0], 75)
+            print pos
+        else:
+            print "Not implemented!!"
+
+
 
 
 
